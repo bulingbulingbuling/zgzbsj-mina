30c00df977ed346e202da8df9794c4866196bbad
liulina <liulina@xiaoyezi.com>
2021-04-26 21:00:44 +0800

    AIPL-14643: [feature] add newindex page

---
+++ src/components/alert20210426.vue
@@ -0,0 +1,378 @@
+<template>
+  <div class="alert" v-if="isShow">
+    <div class="mask"></div>
+    <div class="alert-contact" v-if="type === 'contact'">
+      <img class="alert-contact-close" :src="`${imgPath}/ai_mina/contact-close.png`" alt="" @click="close">
+      <img class="img-bg" :src="`${imgPath}/ai_mina/contact-help.png`">
+      <div class="call" @click="handleCall">现在拨打</div>
+    </div>
+    <div class="alert-form" v-else>
+      <div class='alert-form-tip' >
+        <div class="linkebg">
+          <p class='_p'> 小叶子温馨提示</p>
+          <h3 class="_title">注册后才能参与训练营哦！</h3>
+        </div>
+        <img class="_logo" src="@/static/imgs/tip-logo.png" alt="">
+      </div>
+      <div class="alert-form-item">
+        <div class="num item-left ft-26" @click="handleRedirect">+{{country_code}} <img class="arrow" :src="`${imgPath}/ai_mina/arrow.png`" alt=""></div>
+        <input class="phone ft-26" type="number" placeholder="请输入手机号号码" @input="handleMobileInput">
+      </div>
+      <div class="alert-form-item">
+        <div class="code-title ft-26 item-left">验证码</div>
+        <input class="code ft-26" type="number" placeholder="请输入验证码" @input="handleCodeInput">
+        <div class="code-btn ft-26" :class="{disabled: code < 60 && code > 0}" @click="sendCode">{{smstext || code + 's'}}</div>
+      </div>
+      <p class="alert-form-error ft-26">{{errorText}}</p>
+      <button class="alert-form-btn" @click="login" :disabled="isLogin">立即注册</button>
+      <p class="_decr">* 训练营期间打卡5天还可得<span class="_strong">19.8</span>元学费返现</p>
+      <img class="alert-form-close" src="@/static/imgs/close.png" alt="" @click="close">
+    </div>
+  </div>
+</template>
+<script>
+import { api } from '@/api'
+
+export default {
+  props: {
+    isShow: {
+      type: Boolean
+    },
+    type: {
+      type: String,
+      default: 'contact'
+    },
+    isLogin: {
+      type: Boolean,
+      default: false
+    },
+    form: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  watch: {
+    form(newVal) {
+      if (newVal) {
+        this.mobile = newVal.mobile
+        this.sms_code = newVal.sms_code
+        this.country_code = newVal.country_code || '86'
+      }
+      if (Object.keys(newVal).length === 0) {
+        this.errorText = ''
+      }
+    }
+  },
+  data() {
+    return {
+      mobile: '',
+      sms_code: '',
+      code: '',
+      smstext: '获取验证码',
+      errorText: '',
+      store: this.$mp.app.globalData,
+      country_code: '86',
+      isSending: false,
+      timer: '',
+      imgPath: process.env.VUE_APP_IMG_PATH
+    }
+  },
+  methods: {
+    handleMobileInput(e) {
+      clearTimeout(this.timer)
+      this.timer = setTimeout(() => {
+        this.mobile = e.detail.value
+      }, 200)
+    },
+    handleCodeInput(e) {
+      clearTimeout(this.timer)
+      this.timer = setTimeout(() => {
+        this.sms_code = e.detail.value
+      }, 200)
+    },
+    handleRedirect() {
+      this.store.form = {
+        mobile: this.mobile,
+        sms_code: this.sms_code
+      }
+      this.go('/pages/address')
+    },
+    close() {
+      this.mobile = ''
+      this.sms_code = ''
+      this.code = ''
+      this.errorText = ''
+      this.$emit('close');
+    },
+    handleCall() {
+      wx.makePhoneCall({
+        phoneNumber: '4000292609'
+      })
+    },
+    async sendCode() {
+      if (this.isSending || this.code > 0) return
+      if (!this.mobile) {
+        this.errorText = '*请输入正确的手机号'
+        return
+      }
+      this.isSending = true
+      this.loading()
+      try {
+        await api.sendCode({
+          mobile: this.mobile,
+          country_code: this.country_code
+        })
+        this.setCountTime()
+      } catch (e) {
+      } finally {
+        this.isSending = false
+        this.loading(false);
+      }
+    },
+    // 倒计时
+    setCountTime() {
+      let sum = 60
+      this.code = sum
+      let time = setInterval(() => {
+        this.smstext = ''
+        this.code--
+        if (this.code <= 0) {
+          clearInterval(time);
+          this.smstext = '发送验证码'
+        }
+      }, 1000)
+    },
+    login() {
+      if (!this.mobile || !(/^\d{6,20}$/.test(this.mobile))) {
+        this.errorText = '*请输入正确的手机号'
+        return
+      }
+      if (!this.sms_code || !(/^\d{4,6}$/.test(this.sms_code))) {
+        this.errorText = '*请输入正确的验证码'
+        return
+      }
+      this.errorText = ''
+      this.$emit('login', {
+        mobile: this.mobile,
+        sms_code: this.sms_code,
+        country_code: this.country_code
+      });
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+.alert {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 999;
+  .mask {
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+  }
+  .alert-contact {
+    position: fixed;
+    left: 50%;
+    top: 46%;
+    width: 630px;
+    height: 635px;
+    transform: translate(-50%, -50%);
+    border-radius: 20px;
+    text-align: center;
+    &-close {
+      position: absolute;
+      bottom: -89px;
+      left: 50%;
+      transform: translateX(-50%);
+      width: 56px;
+      height: 56px;
+    }
+    .img-bg {
+      width: 100%;
+      height: 100%;
+    }
+    .call {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+      bottom: 48px;
+      width: 382px;
+      height: 69px;
+      line-height: 69px;
+      background-image: linear-gradient(0deg, #fc5045 0%, #ff7859 100%), linear-gradient(#fc5045, #fc5045);
+      box-shadow: 2px 4px 16px 0px rgba(149, 52, 46, 0.4);
+      border-radius: 34px;
+      text-align: center;
+      letter-spacing: 3px;
+      color: #ffffff;
+      font-size: 28px;
+    }
+  }
+  .alert-form {
+    position: fixed;
+    left: 50%;
+    top: 43%;
+    width: 630px;
+    height: 635px;
+    border-radius: 20px;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    padding: 0px 40px 43px;
+    background-color: #fff;
+    .linkebg {
+      width: 630px;
+      height: 174px;
+      position: relative;
+      overflow: hidden;
+      padding-top: 45px;
+      padding-left: 50px;
+      margin-left: -40px;
+      margin-right: -40px;
+      &::before{
+        content: "";
+        display: block;
+        position: absolute;
+        top: -535px;
+        left: -240px;
+        width: 1071px;
+        height: 1071px;
+        background: rgba(64,213,106,.15);
+        border-radius: 50%;
+        transform: scale(1, 0.322);
+      }
+    }
+    &-close {
+      position: absolute;
+      right: 50%;
+      width: 68px;
+      height: 68px;
+      transform: translate(50%, 90px);
+    }
+    &-tip {
+      color: #333333;
+      text-align: left;
+      position: relative;
+      margin-bottom: 63px;
+      ._p {
+        font-size: 28px;
+        line-height: 41px;
+      }
+      ._title {
+        font-size: 32px;
+      }
+      ._logo {
+        position: absolute;
+        width: 212px;
+        height: 226px;
+        top: -63px;
+        right: -51px;
+      }
+    }
+    &-title {
+      letter-spacing: 1px;
+      color: #444444;
+      font-size: 36px;
+      margin-bottom: 40px;
+    }
+    &-item {
+      display: flex;
+      width: 469px;
+      align-items: center;
+      height: 52px;
+      border-bottom: solid 3px #ddd;
+      margin: 0 auto;
+      margin-bottom: 42px;
+      ::-webkit-input-placeholder {
+        color: #ccc;
+        font-size: 32px;
+      }
+      .item-left {
+        width: 105px;
+        text-align: left;
+        border-right: 3px solid #ccc;
+      }
+      .num {
+        letter-spacing: 1px;
+        color: #444444;
+        .arrow {
+          width: 16px;
+          height: 9px;
+          vertical-align: middle;
+          margin-right: 10px;
+        }
+      }
+      .phone {
+        padding-left: 26px;
+        color: #444;
+        text-align: left;
+        height: 100%;
+        flex: 1;
+        letter-spacing: 0;
+      }
+      .code-title {
+        letter-spacing: 1px;
+        color: #444444;
+        width: 100px;
+      }
+      .code {
+        box-sizing: border-box;
+        width: 230px;
+        text-align: left;
+        padding-left: 26px;
+        color: #444;
+        padding-right: 10px;
+        height: 100%;
+        letter-spacing: 0;
+      }
+      .code-btn {
+        flex: 1;
+        text-align: center;
+        color: #07ae00;
+      }
+      .disabled {
+        color: #999999;
+      }
+      .ft-26 {
+        font-size: 26px;
+      }
+    }
+    &-error {
+      // height: 46px;
+      color: #ff0909;
+      padding-top: 2px;
+      text-align: left;
+      margin: 0 auto;
+      width: 469px;
+      position: relative;
+      top: -30px;
+      height: 24px;
+      font-size: 24px;
+    }
+    &-btn {
+      width: 527px;
+      height: 83px;
+      font-size: 36px;
+      line-height: 83px;
+      color: #ffffff;
+      background-color: #40d56a;
+      border-radius: 41px;
+    }
+    ._decr {
+      font-size: 26px;
+      letter-spacing: -1px;
+      color: #808080;
+      margin-top: 30px;
+      ._strong {
+        font-size: 42px;
+        font-weight: bold;
+        letter-spacing: -4px;
+        color: #ff6a00;
+      }
+    }
+  }
+}
+</style>
--- src/pages/index_20210426.vue
+++ src/pages/index_20210426.vue
@@ -1,45 +1,55 @@
 <template>
   <div class="container index-container">
-    <div class="index-container-content">
-      <img class="header" src="@/static/imgs/newIndex2/bg0.jpg" alt="">
-      <img class="desc" src="@/static/imgs/newIndex2/desc.png" alt="">
-      <swiper class="list-swiper" :circular="true" :vertical="true" :autoplay="true" :interval="1500" v-if="recent_purchase.length > 0">
-        <swiper-item v-for="(item, key) in recent_purchase" :key="key" catchtouchmove="catchTouchMove">
-          <div class="item-wrapper" v-html="item">
+    <div class="index-container-wrapper">
+      <div class="content">
+        <div class="con" v-if="packageOpened">
+          <img class="new-bg-img" src="@/static/imgs/newIndex/opened-new-bg.jpeg" alt="" >
+          <img class="price-img" src="@/static/imgs/newIndex/0.01.png" alt="" v-if="configData.pkg === 3" >
+          <img class="price-img" src="@/static/imgs/newIndex/9.9.png" alt="" v-else >
+        </div>
+        <div class="con" v-else>
+          <img class="new-bg-img" src="@/static/imgs/newIndex/new-bg.jpeg" alt="" >
+          <img class="open-img" src="@/static/imgs/newIndex/open.png" alt="">
+          <div class="open-box" v-if="configData.mobile" @click="handleGetting('拆礼包')" id="mid-btn"></div>
+          <button class="open-box" open-type="getPhoneNumber" @getphonenumber="getPhoneNumber" v-else id="mid-btn" :disabled="hasBtnClicked" @click="handleBtnClick">
+            <div class="open-box" ></div>
+          </button>
+        </div>
+        <div class='btn'>
+          <img class="btn-img" src="@/static/imgs/newIndex/button.png" alt="" v-if="configData.mobile" @click="handleGetting('礼包')">
+          <button class="btn-img" open-type="getPhoneNumber" @getphonenumber="getPhoneNumber" v-else :disabled="hasBtnClicked" @click="handleBtnClick">
+            <img src="@/static/imgs/newIndex/button.png" alt="">
+          </button>
+        </div>
+        <div class="common-sec rules-sec">
+          <a class='rule-link' href='/pages/rules'>*智能陪练购买协议</a>
+        </div>
+      </div>
+      <!-- 支付失败弹窗 -->
+      <div class='stay' v-show="showStay">
+        <div class="mask"></div>
+        <div class="stay-content">
+          <img class="_logo" src="@/static/imgs/sad-logo.png" alt="">
+          <div class='stay-content-title' >
+              <h3 class="_title">您确定要放弃吗？</h3>
+              <p class='_p'> 让孩子跟小叶子一起练琴，每天提高吧～</p>
           </div>
-        </swiper-item>
-      </swiper>
-       <a class='rule-link' href='/pages/rules'>*智能陪练购买协议</a>
-    </div>
-    <div class="index-container-bottom">
-      <div class="action" v-if="configData.mobile" @click="handleGetting('页底立即体验')">
-        <img class="header" src="@/static/imgs/newIndex2/btn-get.png" alt="">
-      </div>
-      <button id="bottom-btn" class="action" open-type="getPhoneNumber" @getphonenumber="getPhoneNumber" :disabled="hasBtnClicked" @click="handleBtnClick" v-else>
-        <img src="@/static/imgs/newIndex2/btn-get.png" alt="">
-      </button>
-    </div>
-    <div class='stay' v-show="showStay">
-      <div class="mask"></div>
-      <div class="stay-content">
-        <img class="_logo" src="@/static/imgs/sad-logo.png" alt="">
-        <div class='stay-content-title' >
-            <h3 class="_title">您确定要放弃吗？</h3>
-            <p class='_p'> 让孩子跟小叶子一起练琴，每天提高吧～</p>
-        </div>
-        <div class='stay-content-body'>
-          <img src="@/static/imgs/stay.png" alt="" class='_img'>
-          <p class='_test'><span>5天</span>不限次数体验</p>
-          <p class='_exp'>练1遍，顶10遍</p>
-        </div>
-        <div class="stay-content-btngroup">
-          <button class="_close" @click="closeStay('拒绝')">残忍拒绝</button>
-          <button class="_pay"  @click="handleGetting('继续', true)">继续支付</button>
+          <div class='stay-content-body'>
+            <img src="@/static/imgs/stay.png" alt="" class='_img'>
+            <p class='_test'><span>5天</span>不限次数体验</p>
+            <p class='_exp'>练1遍，顶10遍</p>
+          </div>
+          <div class="stay-content-btngroup">
+            <button class="_close" @click="closeStay('拒绝')">残忍拒绝</button>
+            <button class="_pay"  @click="handleGetting('继续', true)">继续支付</button>
+          </div>
         </div>
       </div>
+      <alert :isShow="showAlert" @close="closeAlert" :type="alertType" :isLogin="isLogin" :form="form" @login="register">
+      </alert>
+      <!-- 客服提示图标 -->
+      <img class="contact" :src="`${imgPath}/ai_mina/contact.png`" alt="" @click="handleShowAlert('contact')">
     </div>
-    <alert :isShow="showAlert" @close="closeAlert" :type="alertType" :isLogin="isLogin" :form="form" @login="register">
-    </alert>
   </div>
 </template>
 
@@ -54,14 +64,11 @@ export default {
       showAlert: false, // 是否显示弹窗
       showStay: false, // 支付失败挽留弹窗
       store: this.$mp.app.globalData,
-      langUrl: `${process.env.VUE_APP_IMG_PATH}/ai_mina/langngAIG.mp4`,
-      girlUrl: `${process.env.VUE_APP_IMG_PATH}/ai_mina/compare.mp4`,
       showBottom: false,
       alertType: 'phone',
       remainNum: Math.floor(Math.random() * (30 - 20) + 20),
       configData: {},
       referrer_info: {},
-      recent_purchase: [], // 滚动列表
       isLogin: false,
       scene: '', // 通过小程序码进来
       source: '', // 记录小程序的入口
@@ -74,14 +81,46 @@ export default {
       top: '',
       platform: '',
       btnUrl: '',
+      share_type: '',
+      user_status: '',
+      usersignin_status: '',
       imgPath: process.env.VUE_APP_IMG_PATH,
-      shareScene: '' // 分享给他人的scen
+      shareScene: '', // 分享给他人的scen
+      packageOpened: false, // 礼包已打开
+      user_status_map: {
+        0: '未付费',
+        2: '体验期',
+        21: '体验期过期',
+        3: '年卡',
+        31: '年卡过期'
+      }
     }
   },
   onLoad(options) {
     console.log(options, 'options')
     if (options.scene) {
       this.scene = options.scene
+      // 第一版二维码参数格式
+      // options.scene = '%26r%3DzkR2HDs%3D%26c%3D1220'
+      // 第二版二维码参数格式
+      // options.scene = 'UUX2WKF3Q%3D%3D%3D%261351%2611084'
+
+      // 改用从接口获取埋点数据
+      // let scene = decodeURIComponent(options.scene)
+      // const match = scene.match(/c=(\d+)/)
+      // if (match) {
+      //   this.sa.registerApp({
+      //     channel_id: match[1]
+      //   });
+      // } else {
+      //   const params = scene.split('&')
+      //   if (params.length > 0) {
+      //     this.sa.registerApp({
+      //       channel_id: params[1],
+      //       activity_id: params[2]
+      //     });
+      //   }
+      // }
     } else if (options.source) {
       this.source = options.source
     }
@@ -111,10 +150,18 @@ export default {
         this.platform = res.platform
       }
     })
+    // 获取来源
+    const { scene } = wx.getLaunchOptionsSync()
+    this.share_type = scene
+  },
+  onPageScroll(e) {
+    this.showBottom = e.scrollTop > this.btnDistance
+    this.top = e.scrollTop
+  },
+  onReady() {
+    this.getMidBtnInfo()
   },
   onHide() {
-    this.pauseVideo('girl')
-    this.pauseVideo('lang')
     this.form = {}
     this.store.country_code = ''
     this.showAlert = false
@@ -129,8 +176,22 @@ export default {
     }
   },
   methods: {
+    // /点击全屏进入或离开全屏状态触发的事件
+    handleFullscreenChange(e) {
+      if (!e.detail.fullScreen && this.platform !== 'ios') {
+        wx.pageScrollTo({
+          scrollTop: Math.ceil(this.top)
+        })
+      }
+    },
     catchTouchMove() {
       return false
+    },
+    getMidBtnInfo() {
+      // 添加节点的布局位置的查询请求
+      wx.createSelectorQuery().select('#mid-btn').boundingClientRect((rect) => {
+        this.btnDistance = rect.height + rect.top
+      }).exec()
     },
     handleNetError(res) {
       if (res.networkType === 'none') {
@@ -149,6 +210,7 @@ export default {
     },
     closeStay(content) {
       this.showStay = false;
+      this.packageOpened = false
       track('ai_applet_retain_click', {
         ai_tel: this.store.mobile,
         click_item: content
@@ -158,10 +220,11 @@ export default {
       this.hasBtnClicked = true
       let content
       if (e.target.id === 'mid-btn') {
-        content = '页中立即体验'
+        content = '拆礼包'
       } else {
-        content = '页底立即体验'
+        content = '礼包'
       }
+      track('$ai_applet_shouquan_view');
       track('$WebClick', {
         $title: '首页',
         $url: 'pages/index',
@@ -190,7 +253,6 @@ export default {
       console.log('this.configData', this.configData);
       this.btnUrl = this.configData.pkg === 3 ? require('@/static/imgs/btn-1cents.png') : require('@/static/imgs/btn.png')
       this.referrer_info = this.configData.referrer_info
-      this.recent_purchase = this.configData.recent_purchase
       this.shareScene = this.configData.share_scene
       this.store.mobile = this.configData.mobile
       if (this.referrer_info.uuid) {
@@ -218,13 +280,36 @@ export default {
           poster_id: this.configData.scene_data.p
         });
       }
+      if (this.configData.scene_data.user_current_status_zh) {
+        this.sa.registerApp({
+          user_code_status: this.configData.scene_data.user_current_status_zh
+        });
+      }
+      this.sa.registerApp({
+        share_type: this.share_type
+      });
+      // 0是注册用户即未付费，2是体验用户，3是年卡用户 根据sub_end_date(卡过期日期)判断是否过期
+      let user_status_num = this.configData.subscribe_status
+      let date = new Date()
+      let year = date.getFullYear()
+      let month = (date.getMonth() + 1).toString().padStart(2, '0')
+      let day = date.getDate().toString().padStart(2, '0')
+      let dateString = `${year}${month}${day}`
+      if (this.configData.subscribe_status === 2 && dateString / 1 > this.configData.sub_end_date / 1) {
+        user_status_num = '21'
+      }
+      if (this.configData.subscribe_status === 3 && dateString / 1 > this.configData.sub_end_date / 1) {
+        user_status_num = '31'
+      }
       this.sa.registerApp({
         referrer_amount: this.configData.pkg === 3 ? 0.01 : 9.9
       });
       this.sa.setProfile({
         ai_phoneNumber: this.configData.mobile,
         ai_open_id: this.configData.openid,
-        ai_uuid: this.configData.uuid
+        ai_uuid: this.configData.uuid,
+        ai_applet_user_status: this.user_status_map[user_status_num],
+        ai_applet_usersignin_status: this.configData.mobile ? '已登录' : '未登录'
       });
       track('$pageview', {
         $title: '首页',
@@ -245,6 +330,7 @@ export default {
         content,
         visit_time: new Date().toLocaleDateString()
       });
+      this.packageOpened = true
       if (this.configData.had_purchased || (this.store.had_purchased && this.configData.mobile)) {
         this.go('/pages/success')
       } else {
@@ -282,6 +368,7 @@ export default {
           success: () => {
             this.store.had_purchased = true;
             this.go('/pages/success?success=true')
+            // this.getStatus(res.bill.id)
           },
           fail: () => {
             this.form = {}
@@ -302,6 +389,14 @@ export default {
       } finally {
         this.isLogin = false;
         this.showStay = false;
+      }
+    },
+    async getStatus(bill_id) {
+      let res = await api.getBillStatus({
+        bill_id
+      })
+      if (res.bill_status === '1') {
+        this.go('/pages/success?success=true')
       }
     },
     // 手机号注册
@@ -329,6 +424,7 @@ export default {
         }
         this.store.mobile = data.mobile
         this.shareScene = data.share_scene
+        this.packageOpened = true
         if (data.had_purchased) {
           return this.go('/pages/success')
         }
