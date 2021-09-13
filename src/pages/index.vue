00795c3d729371d78ce14210b89e582456e00e70
Ding Bo <dingbo@xiaoyezi.com>
2021-09-13 18:35:54 +0800

    XYZOP-1069: [feature] support 0.01

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -43,7 +43,7 @@
         </div>
       </div>
     </div>
-    <alert :isShow="showAlert" :isFree="showCents" @close="closeAlert" :type="alertType" :isLogin="isLogin" :form="form" @login="register">
+    <alert :isShow="showAlert" :isFree="showCents" :pkg="configData.pkg" @close="closeAlert" :type="alertType" :isLogin="isLogin" :form="form" @login="register">
     </alert>
   </div>
 </template>
@@ -253,7 +253,7 @@ export default {
         }
       }
       this.showCents = this.configData.pkg === 6
-      this.bgUrl = this.showCents ? `${this.imgPath}/abtest/ai_referral_mina/default0.png?v=1` : `${this.imgPath}/ai_mina/newIndex2/default99V1.png`
+      this.bgUrl = this.showCents ? `${this.imgPath}/abtest/ai_referral_mina/default0.png?v=1` : (this.configData.pkg === 3 ? `${this.imgPath}/ai_mina/newIndex2/default001.png` : `${this.imgPath}/ai_mina/newIndex2/default99V1.png`)
       // String 类型试验（第二个参数 ""，表示未命中试验时，会返回此默认值，请根据业务需要更改此处的值）
       if (this.showCents) {
         let that = this
@@ -379,7 +379,7 @@ export default {
         content,
         visit_time: new Date().toLocaleDateString()
       });
-      const ssQuery = `referrer_amount=${this.configData.pkg === 6 ? 0 : 9.9}&channel_id=${this.configData.scene_data.c}`
+      const ssQuery = `referrer_amount=${this.configData.pkg === 6 ? 0 : (this.configData.pkg === 3 ? 0.01 : 9.9)}&channel_id=${this.configData.scene_data.c}`
       if (this.configData.had_purchased === 1 || (this.store.had_purchased === 1 && this.configData.mobile)) {
         this.go(`/pages/success?success=true&${ssQuery}`)
       } else if (this.configData.had_purchased === 2 || (this.store.had_purchased === 2 && this.configData.mobile)) {
